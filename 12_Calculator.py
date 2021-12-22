8c85ee66554265d770e572449380ab1eaddf6682
rakhila123 <95964591+rakhila123@users.noreply.github.com>
2021-12-22 16:16:43 +0600

    Добавление поля результата

--- 12_Calculator.py
+++ 12_Calculator.py
@@ -10,11 +10,14 @@ first = tk.Label(text = "Первое число")
 first.grid(column=0, row=0, padx=15, pady=20)
 second = tk.Label(text= "Второе число") 
 second.grid(column=0, row=1)
+result=tk.Label(text="Результат")
+result.grid(column=0, row=2, padx=15, pady=20)
 
 firstEntry = tk.Entry()
 firstEntry.grid(column=1, row=0)
 secondEntry = tk.Entry() 
 secondEntry.grid(column=1, row=1)
-
+resultLabel = tk.Label(text= "")
+resultLabel.grid(column=1,row=2)
 
 window.mainloop()
